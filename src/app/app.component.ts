df77e882d172fd2257512192b8043b1cfcf467ed
MaR <tuanlinhtrinh91@gmail.com>
2023-04-26 22:09:35 +0700

    Finish Section 33

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AppService } from './app.component.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  masterData: any
+  dataSelected: any
   title = 'learn-app';
+
+  constructor (private dataLesson: AppService, private dataSelect: AppService) {
+  }
+
+  ngOnInit(): void {
+    this.masterData = this.dataLesson
+    this.dataSelect = this.dataSelect
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.component.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isClick = [false, false]
+  courseSelected: any
+
+  constructor (private courseSelect: AppService) {}
+
+  ngOnInit(): void {
+    this.courseSelect = this.courseSelect
+  }
+
+  onTest (a:any) {
+    this.courseSelected = a.id
+    console.log(this.courseSelected)
+  }
 
   onToggle(a: any) {
     if (a === "btn1") {this.isClick[0] = !this.isClick[0]}
